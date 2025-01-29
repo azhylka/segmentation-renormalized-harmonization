6a4d9d49ea29f822bc44643854c7e40f283f1b55
Andrey Zhylka <hotsmileband@gmail.com>
2025-01-29 11:42:42 +0100

    Reformatting training

--- mains/train_cyclegan.py
+++ mains/train_cyclegan.py
@@ -1,7 +1,9 @@
 import os, sys
-
-sys.path.append('../')
-from data.example_loader import h5lodaer
+import os.path as osp
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+base_dir = 'C:/Users/azhylka/Projects/segmentation-renormalized-harmonization/'
+sys.path.append(base_dir)
+from data.rms_loader import RMSloader
 
 from configs.train_options import TrainOptions
 
@@ -19,9 +21,86 @@ import matplotlib
 matplotlib.use('Agg')
 from utils.utilization import mkdir, mkdirs
 from utils.visualization import tensorboard_vis, create_group_fig
-from utils.evaluation import eval_segmentation_batch
+# from utils.evaluation import eval_segmentation_batch
 from models.networks2d import SingleSemanticDropout, PairedSemanticDropout
 import shutil
+
+def visualize_step(writer, losses, step, stage):
+    print('TENSORBOARD VISUALIZATION')
+    writer.add_scalar('train/Loss_G_A', losses['G_A'], step)
+    writer.add_scalar('train/Loss_D_A', losses['D_A'], step)
+    writer.add_scalar('train/Loss_G_B', losses['G_B'], step)
+    writer.add_scalar('train/Loss_D_B', losses['D_B'], step)
+    writer.add_scalar('train/Loss_cycle_A', losses['cycle_A'], step)
+    writer.add_scalar('train/Loss_cycle_B', losses['cycle_B'], step)
+    if opt.lambda_identity > 0:
+        writer.add_scalar('train/Loss_idt_A', losses['idt_A'], step)
+        writer.add_scalar('train/Loss_idt_B', losses['idt_B'], step)
+    if opt.lambda_cc > 0:
+        writer.add_scalar('train/cc_AB', losses['cc_AB'], step)
+        writer.add_scalar('train/cc_BA', losses['cc_BA'], step)
+
+    if opt.joint_seg: #opt.spade and
+        writer.add_scalar('train/seg_A_realA', losses['seg_real_A'], step)
+        writer.add_scalar('train/seg_A_fakeA', losses['seg_fake_A'], step)
+        writer.add_scalar('train/seg_B_fakeB', losses['seg_fake_B'], step)
+        writer.add_scalar('train/seg_B_realB', losses['seg_real_B'], step)
+
+    randind = np.random.randint(model.real_A.size(0))
+    real_A = model.real_A.detach().cpu().numpy()[randind][0]
+    real_B = model.real_B.detach().cpu().numpy()[randind][0]
+    fake_A = model.fake_A.detach().cpu().numpy()[randind][0]
+    fake_B = model.fake_B.detach().cpu().numpy()[randind][0]
+    rec_A = model.rec_A.detach().cpu().numpy()[randind][0]
+    rec_B = model.rec_B.detach().cpu().numpy()[randind][0]
+    print('Visualizing CycleGAN', fake_B.min(), fake_B.max())
+
+    imgs_vis = [real_A, fake_B, rec_A, real_B, fake_A, rec_B]
+    imgs_titles = ['real_A', 'fake_B', 'rec_A', 'real_B', 'fake_A', 'rec_B']
+
+    writer = tensorboard_vis(summarywriter=writer, step=step, board_name=f'{stage}/cycle_crops',
+                                num_row=2, img_list=imgs_vis, cmaps='gist_gray', titles=imgs_titles,
+                                resize=True)
+
+
+    def post_process_seg_out(net_output, batch_idx, prob_seg):
+        if prob_seg is False:
+            seg_out = torch.softmax(net_output, dim=1)
+            seg_out = seg_out.detach().cpu().numpy()[batch_idx].transpose(1, 2, 0)
+            seg_vis = np.argmax(seg_out, axis=-1).astype(np.float32)
+        else:
+            seg_out = torch.softmax(net_output, dim=1)
+            seg_vis = seg_out.detach().cpu().numpy()[batch_idx].transpose(1, 2, 0)
+            seg_vis = seg_vis[:, :, 1:]
+        return seg_vis
+
+
+    if opt.joint_seg:
+        # fig = plt.figure()
+        cmap = matplotlib.colors.ListedColormap(['black', 'red', 'green', 'blue'])
+        print('Visualizing Segmentation')
+        seg_A_gt = model.seg_A.detach().cpu().numpy()[randind].transpose(1, 2, 0)
+        seg_B_gt = model.seg_B.detach().cpu().numpy()[randind].transpose(1, 2, 0)  # w, h, 4
+        if opt.prob_seg:
+            seg_A_gt, seg_B_gt = seg_A_gt[:, :, 1:], seg_B_gt[:, :, 1:]
+        else:
+            seg_A_gt = np.argmax(seg_A_gt, axis=-1).astype(np.float32)
+            seg_B_gt = np.argmax(seg_B_gt, axis=-1).astype(np.float32)
+
+        real_A_seg_out = post_process_seg_out(model.seg_real_A_out, randind, opt.prob_seg)
+        real_B_seg_out = post_process_seg_out(model.seg_real_B_out, randind, opt.prob_seg)
+        fake_A_seg_out = post_process_seg_out(model.seg_fake_A_out, randind, opt.prob_seg)
+        fake_B_seg_out = post_process_seg_out(model.seg_fake_B_out, randind, opt.prob_seg)
+
+        img_seg_vis = [real_A, real_A_seg_out, fake_B, fake_B_seg_out, seg_A_gt,
+                        real_B, real_B_seg_out, fake_A, fake_A_seg_out, seg_B_gt]
+        img_seg_tis = ['real_A', 'real_A_seg', 'fake_B', 'fake_B_seg', 'seg_A_gt',
+                        'real_B', 'real_B_seg', 'fake_A', 'fake_A_seg', 'seg_B_gt']
+        img_seg_map = [cmap if 'seg' in s else 'gist_gray' for s in img_seg_tis]
+
+        writer = tensorboard_vis(writer, step, f'{stage}/seg_cycle_crops',
+                                    num_row=2, img_list=img_seg_vis, cmaps=img_seg_map, titles=img_seg_tis,
+                                    resize=True)
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()
@@ -33,16 +112,23 @@ if __name__ == '__main__':
     if not os.path.exists(summarypath):
         mkdirs(summarypath)
 
-    assert slice_num == opt.input_nc
+    # assert slice_num == opt.input_nc
 
-    train_dataset = h5lodaer('../datasets/train.h5', crop_size=opt.crop_size, src_key='scanner1', trg_key='scanner2')
-    shutil.copy('../scripts/train_cyclegan.sh',
-                os.path.join(summarypath, 'train_cyclegan.sh'))  # copy config file to output folder
+    train_dataset = RMSloader(osp.join(base_dir, 'datasets/train.h5'),  src_key='src', trg_key='trg', is_train=True)
+    val_dataset = RMSloader(osp.join(base_dir, 'datasets/val.h5'),  src_key='src', trg_key='trg', is_train=True)
+    # shutil.copy('../scripts/train_cyclegan.sh',
+    #             os.path.join(summarypath, 'train_cyclegan.sh'))  # copy config file to output folder
     print('Training set: %d' % (train_dataset.__len__()))
     data_loader_train = torch.utils.data.DataLoader(dataset=train_dataset,
                                                     batch_size=opt.batch_size, shuffle=True,
                                                     num_workers=4,
                                                     pin_memory=True)
+    print('Validation set: %d' % (val_dataset.__len__()))
+    data_loader_val = torch.utils.data.DataLoader(dataset=val_dataset,
+                                                    batch_size=opt.batch_size, shuffle=True,
+                                                    num_workers=4,
+                                                    pin_memory=True)
+    
     model = CycleGAN(opt)
     model.save_dir = summarypath
 
@@ -70,104 +156,47 @@ if __name__ == '__main__':
             model.optimize_parameters(step)
             losses = model.get_current_losses()
 
-            print('Src %s, Trg %s, Step %s [Epoch %s/%s Iteration %s/%s] (lr G/D %.5f %.5f):' % (src_scan['prefix'], trg_scan['prefix'],\
-            step, epoch, opt.niter + opt.niter_decay,
-            iteration, len(data_loader_train),
-            model.optimizer_G.param_groups[0]['lr'],
-            model.optimizer_D.param_groups[0]['lr']), losses)
+            # print('Src %s, Trg %s, Step %s [Epoch %s/%s Iteration %s/%s] (lr G/D %.5f %.5f):' % (src_scan['prefix'], trg_scan['prefix'],
+            #                                                                                     step, epoch, opt.niter + opt.niter_decay,
+            #                                                                                     iteration, len(data_loader_train),
+            #                                                                                     model.optimizer_G.param_groups[0]['lr'],
+            #                                                                                     model.optimizer_D.param_groups[0]['lr']), losses)
+            print('Src -> Trg, Step %s [Epoch %s/%s Iteration %s/%s] (lr G/D %.5f %.5f):' % (step, epoch, opt.niter + opt.niter_decay,
+                                                                                                iteration, len(data_loader_train),
+                                                                                                model.optimizer_G.param_groups[0]['lr'],
+                                                                                                model.optimizer_D.param_groups[0]['lr']), losses)
 
             if step % 100 == 0:
-                print('TENSORBOARD VISUALIZATION')
-                writer.add_scalar('train/Loss_G_A', losses['G_A'], step)
-                writer.add_scalar('train/Loss_D_A', losses['D_A'], step)
-                writer.add_scalar('train/Loss_G_B', losses['G_B'], step)
-                writer.add_scalar('train/Loss_D_B', losses['D_B'], step)
-                writer.add_scalar('train/Loss_cycle_A', losses['cycle_A'], step)
-                writer.add_scalar('train/Loss_cycle_B', losses['cycle_B'], step)
-                if opt.lambda_identity > 0:
-                    writer.add_scalar('train/Loss_idt_A', losses['idt_A'], step)
-                    writer.add_scalar('train/Loss_idt_B', losses['idt_B'], step)
-                if opt.lambda_cc > 0:
-                    writer.add_scalar('train/cc_AB', losses['cc_AB'], step)
-                    writer.add_scalar('train/cc_BA', losses['cc_BA'], step)
+                visualize_step(writer, losses, step, 'train')
 
-                if opt.spade and opt.joint_seg:
-                    writer.add_scalar('train/seg_A_realA', losses['seg_real_A'], step)
-                    writer.add_scalar('train/seg_A_fakeA', losses['seg_fake_A'], step)
-                    # writer.add_scalar('train/seg_fake_A', losses['seg_fake_A'], step)
-                    writer.add_scalar('train/seg_B_realB', losses['seg_real_B'], step)
-
-                randind = np.random.randint(model.real_A.size(0))
-                real_A = model.real_A.detach().cpu().numpy()[randind][0]
-                real_B = model.real_B.detach().cpu().numpy()[randind][0]
-                fake_A = model.fake_A.detach().cpu().numpy()[randind][0]
-                fake_B = model.fake_B.detach().cpu().numpy()[randind][0]
-                rec_A = model.rec_A.detach().cpu().numpy()[randind][0]
-                rec_B = model.rec_B.detach().cpu().numpy()[randind][0]
-                print('Visualizing CycleGAN', fake_B.min(), fake_B.max())
-
-                imgs_vis = [real_A, fake_B, rec_A, real_B, fake_A, rec_B]
-                imgs_titles = ['real_A', 'fake_B', 'rec_A', 'real_B', 'fake_A', 'rec_B']
-
-                writer = tensorboard_vis(summarywriter=writer, step=step, board_name='train_new/cycle_crops',
-                                         num_row=2, img_list=imgs_vis, cmaps='gist_gray', titles=imgs_titles,
-                                         resize=True)
-
-
-                def post_process_seg_out(net_output, batch_idx, prob_seg):
-                    if prob_seg is False:
-                        seg_out = torch.softmax(net_output, dim=1)
-                        seg_out = seg_out.detach().cpu().numpy()[batch_idx].transpose(1, 2, 0)
-                        seg_vis = np.argmax(seg_out, axis=-1).astype(np.float32)
-                    else:
-                        seg_out = torch.softmax(net_output, dim=1)
-                        seg_vis = seg_out.detach().cpu().numpy()[batch_idx].transpose(1, 2, 0)
-                        seg_vis = seg_vis[:, :, 1:]
-                    return seg_vis
-
-
-                if opt.joint_seg:
-                    # fig = plt.figure()
-                    cmap = matplotlib.colors.ListedColormap(['black', 'red', 'green', 'blue'])
-                    print('Visualizing Segmentation')
-                    seg_A_gt = model.seg_A.detach().cpu().numpy()[randind].transpose(1, 2, 0)
-                    seg_B_gt = model.seg_B.detach().cpu().numpy()[randind].transpose(1, 2, 0)  # w, h, 4
-                    if opt.prob_seg:
-                        seg_A_gt, seg_B_gt = seg_A_gt[:, :, 1:], seg_B_gt[:, :, 1:]
-                    else:
-                        seg_A_gt = np.argmax(seg_A_gt, axis=-1).astype(np.float32)
-                        seg_B_gt = np.argmax(seg_B_gt, axis=-1).astype(np.float32)
-
-                    real_A_seg_out = post_process_seg_out(model.seg_real_A_out, randind, opt.prob_seg)
-                    real_B_seg_out = post_process_seg_out(model.seg_real_B_out, randind, opt.prob_seg)
-                    fake_A_seg_out = post_process_seg_out(model.seg_fake_A_out, randind, opt.prob_seg)
-                    fake_B_seg_out = post_process_seg_out(model.seg_fake_B_out, randind, opt.prob_seg)
-
-                    img_seg_vis = [real_A, real_A_seg_out, fake_B, fake_B_seg_out, seg_A_gt,
-                                   real_B, real_B_seg_out, fake_A, fake_A_seg_out, seg_B_gt]
-                    img_seg_tis = ['real_A', 'real_A_seg', 'fake_B', 'fake_B_seg', 'seg_A_gt',
-                                   'real_B', 'real_B_seg', 'fake_A', 'fake_A_seg', 'seg_B_gt']
-                    img_seg_map = [cmap if 'seg' in s else 'gist_gray' for s in img_seg_tis]
-
-                    writer = tensorboard_vis(writer, step, 'train_new/seg_cycle_crops',
-                                             num_row=2, img_list=img_seg_vis, cmaps=img_seg_map, titles=img_seg_tis,
-                                             resize=True)
-
-                    '''
-                    Batch Evaluation
-                    '''
-                    print('----------src_seg_on_src_img----------- ')
-                    acc_real_A, pre_real_A, rec_real_A, f1_real_A = eval_segmentation_batch(model.seg_real_A_out,
-                                                                                            model.seg_A, opt.dataset)
-                    print('----------trg_seg_on_hrm_img----------- ')
-                    acc_fake_B, pre_fake_B, rec_fake_B, f1_fake_B = eval_segmentation_batch(model.seg_fake_B_out,
-                                                                                            model.seg_A, opt.dataset)
-                    print('----------trg_seg_on_trg_img----------- ')
-                    acc_real_B, pre_real_B, rec_real_B, f1_real_B = eval_segmentation_batch(model.seg_real_B_out,
-                                                                                            model.seg_B, opt.dataset)
+                    # '''
+                    # Batch Evaluation
+                    # '''
+                    # print('----------src_seg_on_src_img----------- ')
+                    # acc_real_A, pre_real_A, rec_real_A, f1_real_A = eval_segmentation_batch(model.seg_real_A_out,
+                    #                                                                         model.seg_A, opt.dataset)
+                    # print('----------trg_seg_on_hrm_img----------- ')
+                    # acc_fake_B, pre_fake_B, rec_fake_B, f1_fake_B = eval_segmentation_batch(model.seg_fake_B_out,
+                    #                                                                         model.seg_A, opt.dataset)
+                    # print('----------trg_seg_on_trg_img----------- ')
+                    # acc_real_B, pre_real_B, rec_real_B, f1_real_B = eval_segmentation_batch(model.seg_real_B_out,
+                                                                                            # model.seg_B, opt.dataset)
                     # print('----------trg_seg_on_src_img----------- ')
-                    # acc_fake_A, pre_fake_A, rec_fake_A, f1_fake_A = eval_segmentation_batch(model.seg_real_A_out, model.seg_A, opt.dataset)
-
+                    # acc_fake_A, pre_fake_A, rec_fake_A, f1_fake_A = eval_segmentation_batch(model.seg_real_A_out, model.seg_A, opt.dataset) 
+        with torch.no_grad():
+            acc_losses = {}
+            for iteration, data in enumerate(data_loader_val):
+                model.set_input(data)
+                losses = model.get_current_losses()
+                for loss_key, loss_val in losses.items():
+                    if loss_key not in acc_losses:
+                        acc_losses[loss_key] = []
+                    acc_losses[loss_key].append(loss_val)
+            losses = {}
+            for loss_key, loss_vals in acc_losses.items():
+                losses[loss_key] = np.mean(loss_vals)
+            visualize_step(writer, losses, step, "val")
+        
         model.update_learning_rate()
         if (epoch) % opt.save_epoch_freq == 0:  # cache our latest model every <save_latest_freq> iterations
             print('saving model (epoch %d, total_iters %d)' % (epoch, step))
